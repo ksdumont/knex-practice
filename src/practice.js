7a1095677f4976384ac9406d537e4c7bedcf8b56
ksdumont <ksdumont@gmail.com>
2019-12-23 05:56:49 -0800

    knex instance

--- src/practice.js
+++ src/practice.js
@@ -5,4 +5,12 @@ const knexInstance = knex({
     client: 'pg',
     connection: process.env.DB_URL
 })
-console.log('knex and driver installed correctly');
+
+const q1 = knexInstance('amazong_products').select('*')
+.toQuery();
+
+const q2 = knexInstance.from('amazong_products').select('*')
+.toQuery();
+
+console.log('q1:', q1);
+console.log('q2:', q2);
